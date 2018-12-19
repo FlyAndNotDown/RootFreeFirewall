68d0175fb7792ec576e4ed7671932d1b97094d37
John Kindem <461425614@qq.com>
2018-12-19 19:05:15 +0800

    [update] update a config file

--- app/src/main/java/com/nuaa/is/rootfreefirewall/VpnInterface.java
+++ app/src/main/java/com/nuaa/is/rootfreefirewall/VpnInterface.java
@@ -1,14 +1,8 @@
 package com.nuaa.is.rootfreefirewall;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.VpnService;
-import android.os.Binder;
-import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
-import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
