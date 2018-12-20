39d65a0fc1e637f482c8a4ae74a9e5b19d120727
John Kindem <461425614@qq.com>
2018-12-20 15:46:26 +0800

    [add] add some code

--- app/src/main/java/com/nuaa/is/rootfreefirewall/service/FirewallVpnService.java
+++ app/src/main/java/com/nuaa/is/rootfreefirewall/service/FirewallVpnService.java
@@ -16,8 +16,11 @@ import com.nuaa.is.rootfreefirewall.net.UDPOutput;
 
 import java.io.Closeable;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.Selector;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
@@ -137,11 +140,11 @@ public class FirewallVpnService extends VpnService {
         this.deviceToNetworkTCPQueue = null;
         this.networkToDeviceQueue = null;
         ByteBufferPool.clear();
-        closeReousrce(udpSelector, tcpSelector, parcelFileDescriptor);
+        FirewallVpnService.closeResource(udpSelector, tcpSelector, parcelFileDescriptor);
     }
 
     // 清理资源
-    private void closeReousrce(Closeable... resources) {
+    private static void closeResource(Closeable... resources) {
         for (Closeable resource : resources) {
             try {
                 resource.close();
@@ -153,7 +156,7 @@ public class FirewallVpnService extends VpnService {
     }
 
     // Vpn 服务线程
-    public static class VPNRunnable implements Runnable {
+    private static class VPNRunnable implements Runnable {
 
         private static final String TAG = "RFF-VpnThread";
 
@@ -180,7 +183,98 @@ public class FirewallVpnService extends VpnService {
 
         @Override
         public void run() {
-            // TODO
+            Log.i(VPNRunnable.TAG, "VPN thread started");
+
+            // 获取 channel
+            FileChannel vpnInput = new FileInputStream(fileDescriptor).getChannel();
+            FileChannel vpnOutput = new FileOutputStream(fileDescriptor).getChannel();
+
+            try {
+                // 创建发送缓冲区
+                ByteBuffer bufferToNetwork = null;
+                boolean dataSent = true;
+                boolean dataReceived;
+
+                // 开始循环
+                while (!Thread.interrupted()) {
+                    if (dataSent) {
+                        // 如果已经发送了数据，则从缓冲池中获取一个缓冲区
+                        bufferToNetwork = ByteBufferPool.acquire();
+                    } else {
+                        // 如果还没发送，则先清空
+                        bufferToNetwork.clear();
+                    }
+
+                    // 读取一个来自物理网卡的数据包
+                    int readLength = vpnInput.read(bufferToNetwork);
+                    if (readLength > 0) {
+                        // 如果读取到了数据
+                        Log.i(VPNRunnable.TAG, "get a ip packet");
+                        dataSent = true;
+
+                        // 在读取数据前先将 limit 设置为 position，position 设置为 0
+                        bufferToNetwork.flip();
+
+                        // 拆包
+                        Packet packet = new Packet(bufferToNetwork);
+
+                        // 判断包的种类
+                        if (packet.isUDP()) {
+                            // 如果是 UDP 包
+                            Log.i(VPNRunnable.TAG, "it's a UDP packet");
+                            // 在队列中加入包
+                            deviceToNetworkUDPQueue.offer(packet);
+                        } else if (packet.isTCP()) {
+                            // 如果是 TCP 包
+                            Log.i(VPNRunnable.TAG, "it's a TCP packet");
+                            // 在队列中加入包
+                            deviceToNetworkTCPQueue.offer(packet);
+                        } else {
+                            // 如果是其他包
+                            Log.i(VPNRunnable.TAG, "it's a unknown type packet");
+                            Log.i(VPNRunnable.TAG, packet.ip4Header.toString());
+                            dataSent = false;
+                        }
+                    } else {
+                        // 如果没有读取到数据
+                        dataSent = false;
+                    }
+
+                    // 每次都尝试从收外部网络的队列取走一个数据包
+                    ByteBuffer bufferFromNetwork = networkToDeviceQueue.poll();
+                    // 如果拿到了数据
+                    if (bufferFromNetwork != null) {
+                        // 准备读取
+                        bufferFromNetwork.flip();
+                        // 将数据一股脑写回屋里网卡
+                        while (bufferFromNetwork.hasRemaining()) {
+                            vpnOutput.write(bufferFromNetwork);
+                        }
+                        // 设置收到数据标识
+                        dataReceived = true;
+                        // 从缓冲池中释放缓冲区
+                        ByteBufferPool.release(bufferFromNetwork);
+                    } else {
+                        dataReceived = false;
+                    }
+
+                    // 无收也无发，则让线程睡一会
+                    if (!dataSent && dataReceived) {
+                        Thread.sleep(10);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.e(VPNRunnable.TAG, "get a interrupted exception");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(VPNRunnable.TAG, "get a IO exception");
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e(VPNRunnable.TAG, "get a exception");
+            } finally {
+                FirewallVpnService.closeResource(vpnInput, vpnOutput);
+            }
         }
 
     }
