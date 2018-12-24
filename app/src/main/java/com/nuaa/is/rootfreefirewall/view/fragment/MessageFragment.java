6ffac9d84c115f0ce4bece4a6b47da63908cfe16
John Kindem <461425614@qq.com>
2018-12-25 05:04:52 +0800

    [add] f**k the Android version after 4.4

--- app/src/main/java/com/nuaa/is/rootfreefirewall/view/fragment/MessageFragment.java
+++ app/src/main/java/com/nuaa/is/rootfreefirewall/view/fragment/MessageFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Telephony;
 
 import com.nuaa.is.rootfreefirewall.R;
 
@@ -14,6 +15,8 @@ import com.nuaa.is.rootfreefirewall.R;
  * Message木块 Fragment
  */
 public class MessageFragment extends Fragment {
+
+    private String defaultSmsApplication;
 
     @Nullable
     @Override
@@ -24,5 +27,31 @@ public class MessageFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+
+        this.requestToBeDefaultSmsApplication();
     }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    // 请求成为默认短信应用
+    private void requestToBeDefaultSmsApplication() {
+        // 查询当前的默认短信应用并且将报名存储起来
+        this.defaultSmsApplication = Telephony.Sms.getDefaultSmsPackage(getActivity());
+        // 向用户请求将本应用设置成默认短信应用
+        Intent intent = new Intent(getActivity(), Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getActivity().getPackageName());
+        startActivity(intent);
+    }
+
+    // 还原成原来的短信应用
+    private void recoverDefaultSmsApplication() {
+        Intent intent = new Intent(getActivity(), Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, this.defaultSmsApplication);
+        startActivity(intent);
+    }
+
 }
