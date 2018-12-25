073c15f5b58174021a10f54cce1e20d9c3243bb1
John Kindem <461425614@qq.com>
2018-12-25 14:55:14 +0800

    [add] add some f**king code

--- app/src/main/java/com/nuaa/is/rootfreefirewall/view/fragment/MessageFragment.java
+++ app/src/main/java/com/nuaa/is/rootfreefirewall/view/fragment/MessageFragment.java
@@ -1,13 +1,14 @@
 package com.nuaa.is.rootfreefirewall.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.Telephony;
 
 import com.nuaa.is.rootfreefirewall.R;
 
@@ -16,7 +17,8 @@ import com.nuaa.is.rootfreefirewall.R;
  */
 public class MessageFragment extends Fragment {
 
-    private String defaultSmsApplication;
+    // 默认短信应用
+    private static final String defaultSmsApplication = "com.android.sms";
 
     @Nullable
     @Override
@@ -34,22 +36,21 @@ public class MessageFragment extends Fragment {
 
     @Override
     public void onDestroy() {
-
+        super.onDestroy();
+        this.recoverDefaultSmsApplication();
     }
 
     // 请求成为默认短信应用
     private void requestToBeDefaultSmsApplication() {
-        // 查询当前的默认短信应用并且将报名存储起来
-        this.defaultSmsApplication = Telephony.Sms.getDefaultSmsPackage(getActivity());
         // 向用户请求将本应用设置成默认短信应用
-        Intent intent = new Intent(getActivity(), Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
         intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getActivity().getPackageName());
         startActivity(intent);
     }
 
     // 还原成原来的短信应用
     private void recoverDefaultSmsApplication() {
-        Intent intent = new Intent(getActivity(), Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
         intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, this.defaultSmsApplication);
         startActivity(intent);
     }
